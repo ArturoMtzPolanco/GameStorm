102bfbe4309a2841aea5f8dcc2fa174f60d75728
arturo martinez <20393160@utcancun.edu.mx>
2025-03-04 20:58:23 -0500

    no jala los datos

---
+++ Content/AplicationDbContent .cs
@@ -0,0 +1,34 @@
+﻿using GameStorm.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bibloteca.Context
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options) { }
+
+        //modelos a mapear
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario
+                {
+                    PkUsuario = 1,
+                    Nombre = "admin",
+                    email = "admin@gmail.com",
+                    Password = "123",
+                    FkRol = 1
+                });
+
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                PkRol = 1,
+                Nombre = "Admin"
+            });
+        }
+    }
+}
